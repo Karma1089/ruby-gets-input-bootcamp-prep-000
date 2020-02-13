f904c37e420cb465ccbfb7b8b8f072eb6af7f5b3
Karmelitta Henderson <karma24butts@gmail.com>
2020-02-13 18:19:38 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,4 @@
-def greeting
+def greeting(name
+   to taut now with this oppertunity that I will not squanderke care of my mom and dad. B )
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
